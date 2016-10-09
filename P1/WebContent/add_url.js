28415cb0deff7dc1071b2086fe7e76d337c29077
Donald <d.james.meek@gmail.com>
2016-10-09 12:57:01 -0500

    jQuery works, adding newlines

--- P1/WebContent/add_url.js
+++ P1/WebContent/add_url.js
@@ -1,30 +1,6 @@
-/* Generates text box with name auto-incrimenting */
-class URL() {
-	let URLName;
-	constructor()
-	{
-		URLName = 0;
-	}
-	var getURL = function () {
-		return URLName;
-	}
-	var incrementURL = function() {
-		URLName++;
-	}
+function newURL() {
+	console.log("Here in function");
+	
+	$("input[name='url_name']:first").clone().insertAfter("div.added_url_textbox");
 }
-var getNewURL = function () {
-	var url = new URL();
-	document.getElementById("testing").innerHTML = url.getURL();
-	url.incrementURL();
-	console.log(url.getURL);
-	
-	
-	
-	
-	exit(0);
-	
-	
-	
-	
-	
-}
+
